5080e0943eb91a5da7c57b34e91f19a7ae8fab07
Jayson Harshbarger <hypercubed@gmail.com>
2019-10-28 13:39:48 -0600

    Demo mousover/mouseout issue

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,17 @@
 describe('page', () => {
-  it('works', () => {})
-})
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('works in v3.4, fails in v3.5', () => {
+    cy.get('#btn-1').click();
+    cy.wait(3000);
+    cy.get('#btn-2').click();
+  });
+
+  it('works in v3.4 and v3.5', () => {
+    cy.get('#btn-1').click().trigger('mouseout', { force: true });
+    cy.wait(3000);
+    cy.get('#btn-2').click();
+  });
+});
